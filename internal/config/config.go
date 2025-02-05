de5ee69776839157f80d6803350f34f1bad4b8f0
Nikita <reshetnyaknikita6@gmail.com>
2025-02-05 20:03:49 +0300

    create config

---
+++ internal/config/config.go
@@ -0,0 +1,36 @@
+package config
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type Config struct {
+	Env         string `yaml:"env" env-default:"development"`
+	StoragePath string `yaml:"storage_path" env-required:"true"`
+	HttpServer  `yaml:"http_server"`
+}
+type HttpServer struct {
+	Address     string        `yaml:"address" env-default:"0.0.0.0:8080"`
+	Timeout     time.Duration `yaml:"timeout" env-default:"5s"`
+	IdleTimeout time.Duration `yaml:"idle_timeout" env-default:"60s"`
+}
+
+func MustLoad() *Config {
+	configpath := os.Getenv("CONFIG_PATH")
+	if configpath == "" {
+		log.Fatal("CONFIG_PATH environment variable is not set")
+	}
+	if _, err := os.Stat(configpath); err != nil {
+		log.Fatalf("error opening config file: %s", err)
+	}
+	var cfg Config
+	err := cleanenv.ReadConfig(configpath, &cfg)
+	if err != nil {
+		log.Fatalf("error reading config file: %s", err)
+	}
+	return &cfg
+}
